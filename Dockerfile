b2c819e4b7c6767ccd7a265e61c5241a932f2705
Martijn van Maurik <martijn@vmaurik.nl>
2015-08-25 17:15:59 +0200

    Fix the build

--- Dockerfile
+++ Dockerfile
@@ -1,17 +1,10 @@
 FROM combro2k/ubuntu-debootstrap:14.04
 MAINTAINER Martijn van Maurik <docker@vmaurik.nl>
 
-ENV SCRIPT="/usr/local/bin/install.sh" \
-    APP_HOME="/home/asciinema" \
-    APP_USER="asciinema"
-
 ADD resources/bin/* /usr/local/bin/
 
-RUN chmod +x /usr/local/bin/* && \
-    bash ${SCRIPT} install_custom_repo install_dependencies compile_libtsm create_user
+RUN chmod +x /usr/local/bin/* && /bin/bash -l -c "/usr/local/bin/install.sh build"
 
 USER asciinema
-
-RUN /usr/bash -l -c ${SCRIPT} install_ruby_rvm install_asciinema configure_asciinema
 
 CMD ['/usr/local/bin/run']
--- resources/bin/install.sh
+++ resources/bin/install.sh
@@ -6,118 +6,112 @@ e () {
     echo "error $errorcode"
     echo "the command executing at the time of the error was"
     echo "$BASH_COMMAND"
-    echo "on line ${BASH_LINENO[0]}"
-    #tail -n 25 ${INSTALL_LOG}
+    echo "on line ${BASH_LINENO[0]}" # && tail -n 25 ${INSTALL_LOG}
     exit 1  # or use some other value or do return instead
 }
 
 trap e ERR
 
-ASCIICEMA_SERVER="${APP_HOME}/asciinema.org"
-DEBIAN_FRONTEND=noninteractive
-TMP_DIR="$(mktemp -u -d -t tsmXXXXXX)"
-PACKAGES=(
-    'autoconf'
-    'automake'
-    'bison'
-    'curl'
-    'g++'
-    'gawk'
-    'gcc'
-    'git'
-    'libc6-dev'
-    'libffi-dev'
-    'libgdbm-dev'
-    'libncurses5-dev'
-    'libreadline6-dev'
-    'libsqlite3-dev'
-    'libssl-dev'
-    'libtool'
-    'libyaml-dev'
-    'make'
-    'patch'
-    'phantomjs'
-    'pkg-config'
-    'sqlite3'
-    'sudo'
-    'zlib1g-dev'
-)
+export  \
+    CUR_USER="$(id -un)"
+    CUR_UID="$(id -u)"
+    DEBIAN_FRONTEND="noninteractive" \
+    APP_USER="asciinema" \
+    APP_HOME="/home/${APP_USER}" \
+    ASCIICEMA_SERVER="${APP_HOME}/asciinema-server" \
+    TMP_DIR="$(mktemp -u -d -t tsmXXXXXX)" \
+    PACKAGES=(
+        'autoconf'
+        'automake'
+        'bison'
+        'curl'
+        'g++'
+        'gawk'
+        'gcc'
+        'git'
+        'libc6-dev'
+        'libffi-dev'
+        'libgdbm-dev'
+        'libncurses5-dev'
+        'libreadline6-dev'
+        'libsqlite3-dev'
+        'libssl-dev'
+        'libtool'
+        'libyaml-dev'
+        'make'
+        'patch'
+        'phantomjs'
+        'pkg-config'
+        'sqlite3'
+        'sudo'
+        'zlib1g-dev'
+    )
 
+# root user functions
 create_user(){
-    id "${APP_USER}" > /dev/null 2>&1 && return 0
-    [[ "${UID}" -ne 0 ]] && echo "Need to be root to run this command!" && return 1;
+    [ "${CUR_UID}" -ne 0 ] && echo "Need to be root to run ${FUNCNAME[0]} (running as ${CUR_USER})" && exit 1
 
-    echo "Creating user ${APP_USER}..."
-    useradd -d "${APP_HOME}" -m -s "/bin/bash" "${APP_USER}"
-    echo "${APP_USER} = NOPASSWD: ALL" > "/etc/sudoers.d/{APP_USER}"
+    if ! id -u "${APP_USER}" >/dev/null 2>&1
+    then
+        echo "Creating user ${APP_USER}..."
+        useradd -d "${APP_HOME}" -m -s "/bin/bash" "${APP_USER}"
+        echo "${APP_USER} ALL=(ALL) NOPASSWD:ALL" > /etc/sudoers.d/${APP_USER}
+    fi
 }
 
 compile_libtsm() {
-    [ -e "/usr/local/lib/libtsm.a" ] && return 0
-    [[ "${UID}" -ne 0 ]] && echo "Need to be root to run this command!" && return 1;
+    [ "${CUR_UID}" -ne 0 ] && echo "Need to be root to run ${FUNCNAME[0]} (running as ${CUR_USER})" && exit 1
 
-    echo "Compiling libtsm-3"
-    git clone -q git://people.freedesktop.org/~dvdhrm/libtsm "${TMP_DIR}"
-    pushd "${TMP_DIR}"
+    if [ ! -f "/usr/local/lib/libtsm.a" ]
+    then
+        echo "Compiling libtsm-3"
 
-    git checkout libtsm-3
-    test -f ./configure || NOCONFIGURE=1 ./autogen.sh
-    ./configure --prefix=/usr/local
-    make
-    make install
-    popd
+        mkdir -p "${TMP_DIR}"
+        pushd "${TMP_DIR}"
 
-    rm -fr "${TMP_DIR}"
+        git clone -q git://people.freedesktop.org/~dvdhrm/libtsm .
+        git checkout libtsm-3
 
-    return 0
-}
+        if [ ! -f "./configure" ]
+        then
+            NOCONFIGURE=1 ./autogen.sh
+        fi
 
-install_custom_repo()
-{
-    [[ "${UID}" -ne 0 ]] && echo "Need to be root to run this command!" && return 1;
+        ./configure --prefix=/usr/local
+        make
+        make install
 
-    [ ! -f "/usr/bin/add-apt-repository" ] && apt-get update && apt-get install -yq python-software-properties software-properties-common
-
-    add-apt-repository -y ppa:tanguy-patte/phantomjs
-    apt-get update
+        popd
+        rm -fr "${TMP_DIR}"
+    fi
 
     return 0
 }
 
 install_dependencies() {
-    [[ "${UID}" -ne 0 ]] && echo "Need to be root to run this command!" && return 1;
+    [ "${CUR_UID}" -ne 0 ] && echo "Need to be root to run ${FUNCNAME[0]} (running as ${CUR_USER})" && exit 1
+
+    if [ ! -f "/usr/bin/add-apt-repository" ]
+    then
+        apt-get update
+        apt-get install -yq python-software-properties software-properties-common
+    fi
+
+    add-apt-repository -y ppa:tanguy-patte/phantomjs
+    apt-get update
 
     apt-get install -yq ${PACKAGES[@]}
 
-    return 0
-}
-
-install_ruby_rvm() {
-    [ -d "${APP_HOME}/.rvm/" ] && return 1
-    [[ "${USER}" != "${APP_USER}" ]] && echo "Need to be ${APP_USER} to run this command" && return 1;
-
-    echo "Installing RVM ruby..."
-    gpg --keyserver hkp://keys.gnupg.net --recv-keys D39DC0E3
-    curl -L get.rvm.io | bash -s stable --rails
-    rvm install 2.1
-    echo 'gem: --no-ri --no-rdoc' > ${APP_HOME}/.gemrc
-    gem install bundler rake --no-ri --no-rdoc
-
-    return 0
-}
-
-install_asciinema() {
-    [ -d "${ASCIICEMA_SERVER}" ] && return 1
-    [[ "${USER}" != "${APP_USER}" ]] && echo "Need to be ${APP_USER} to run this command" && return 1;
-
-    echo "Checking out asciinema.org..."
-    git -q clone git://github.com/asciinema/asciinema.org.git "${ASCIICEMA_SERVER}"
+    if [ ! -f "/usr/local/lib/libtsm.a" ]
+    then
+        compile_libtsm
+    fi
 
     return 0
 }
 
 chown_asciinema() {
-    [[ "${UID}" -ne 0 ]] && echo "Need to be root to run this command!" && return 1;
+    [ "${CUR_UID}" -ne 0 ] && echo "Need to be root to run ${FUNCNAME[0]} (running as ${CUR_USER})" && exit 1
 
     echo "Setting the correct user rights on ${ASCIICEMA_SERVER}..."
     chown -R "${APP_USER}:${APP_USER}" "${ASCIICEMA_SERVER}"
@@ -125,9 +119,46 @@ chown_asciinema() {
     return 0
 }
 
+
+# asciinema specific user functions
+install_asciinema() {
+    [[ "${CUR_USER}" != "${APP_USER}" ]] && echo "Need to be ${APP_USER} to run ${FUNCNAME[0]} (running as ${CUR_USER})" && exit 1
+
+    if [ ! -d "${APP_HOME}/.rvm/" ]
+    then
+        install_ruby_rvm
+    fi
+
+    if [ ! -d "${ASCIICEMA_SERVER}" ]
+    then
+        echo "Checking out asciinema.org..."
+        curl -sL https://github.com/asciinema/asciinema.org/archive/master.tar.gz | tar zx -C "${ASCIICEMA_SERVER}" --strip-components=1
+    fi
+
+    configure_asciinema
+
+    return 0
+}
+
+install_ruby_rvm() {
+    [[ "${CUR_USER}" != "${APP_USER}" ]] && echo "Need to be ${APP_USER} to run ${FUNCNAME[0]} (running as ${CUR_USER})" && exit 1
+
+    if [ ! -d "${APP_HOME}/.rvm/" ]
+    then
+        echo "Installing RVM ruby..."
+        gpg --keyserver hkp://keys.gnupg.net --recv-keys D39DC0E3
+        curl -L get.rvm.io | bash -s stable --rails
+        rvm install 2.1
+        echo 'gem: --no-ri --no-rdoc' > ${APP_HOME}/.gemrc
+        gem install bundler rake --no-ri --no-rdoc
+    fi
+
+    return 0
+}
+
 configure_asciinema() {
-    [[ "${USER}" != "${APP_USER}" ]] && echo "Need to be ${APP_USER} to run this command" && return 1;
-    [ ! -d "${ASCIICEMA_SERVER}" ] && install_asciinema
+    [[ "${CUR_USER}" != "${APP_USER}" ]] && echo "Need to be ${APP_USER} to run ${FUNCNAME[0]} (running as ${CUR_USER})" && exit 1
+
     source "${APP_HOME}/.rvm/scripts/rvm"
 
     [ ! -e "/data/config/database.yml" ] && echo "Copying base skeleton" && cp "${ASCIICEMA_SERVER}/config/database.yml.example" "/data/config/database.yml"
@@ -145,9 +176,16 @@ configure_asciinema() {
     return 0
 }
 
+build() {
+    install_dependencies
+    create_user
+
+    sudo -H -u "${APP_USER}" ${0} install_asciinema
+}
+
 if [ $# -eq 0 ]
 then
-    echo "No parameters given!"
+    echo "No parameters given! (${@})"
     echo "Available functions:"
     echo
 
@@ -157,7 +195,7 @@ then
 else
     for a in ${@}
     do
-        ${a} || exit 1
+        ${a}
     done
 fi
 
