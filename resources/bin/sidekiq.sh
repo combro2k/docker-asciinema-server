0c6fb0fd625ecc8edfc47f557a25fb239c11998f
Martijn van Maurik <martijn@vmaurik.nl>
2015-10-31 12:49:42 +0100

    Refactoring

---
+++ resources/bin/sidekiq.sh
@@ -0,0 +1,19 @@
+#!/bin/bash -e
+
+export RAILS_ENV=production
+
+load_rvm()
+{
+	if [ -f "${HOME}/.rvm/scripts/rvm" ]
+	then
+		source ${HOME}/.rvm/scripts/rvm
+	else
+		echo "Could not load RVM"
+		exit 1
+	fi
+}
+
+load_rvm
+
+cd ${HOME}/server
+bundle exec sidekiq -e production
